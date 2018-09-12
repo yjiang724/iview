b01ae7a8eb9c2b2f555334478590ff2fe5da411b
nathanjiangyu <nathanjiangyu@didichuxing.com>
2018-09-12 11:02:34 +0800

    fix

--- dist/iview.js
+++ dist/iview.js
@@ -25083,7 +25083,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '2.14.2',
+    version: '3.0.9',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
